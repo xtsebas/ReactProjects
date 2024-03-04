cc81a28fae4755c54cc6f4a6067359c47ed9b25a
Sebastian <shuertas@teknovate.biz-at-172302277217>
2024-03-04 13:48:34 -0600

    feat: components with css

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import Core from './components/Core.jsx';
+import TabButton from './components/TabButon.jsx';
 
 function App() {
   return (
@@ -16,7 +17,16 @@ function App() {
             <Core {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+
+          </menu>
+        </section>
       </main>
     </div>
   );
